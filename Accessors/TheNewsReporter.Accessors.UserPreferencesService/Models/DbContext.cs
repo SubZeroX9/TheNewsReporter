2e3aa4eceae767fae1ee54bf3c6b1edf1a5d9920
Rafael Azriaiev <84143075+SubZeroX9@users.noreply.github.com>
2024-07-06 19:10:48 +0300

    First Version of API with Simple user preference fetcherAccessor and mongoDB (#1)
    * Created UserPreferences Accessor
    
    - created new ASP.NET core solution for user preferences accessor
    - created docker compose file with accessor and mongodb services
    - added mongo db seeder for user preferences
    - defined mongo database in app settings of user prefs accessor
    
    * Updated The User Preferences service
    
    - Removed Wether forcast template
    - Created Mongo Db connection
    - updated docker-compose to work with user preference service and mongo service on news report network with volume for mongo db
    - create The following request:
       - get all users with thier prefferences
       - get specific users preferences
       - add a new user with his preferences
       - delete a user and his preferences
       - updates a user and his preferences
    - updated seeder to match UserPreferences class and document
    
    * Update to public instead of internal for testing
    
    * Fixed endpoint inconsistency
    
    * Update local Readme file for user preference service

---
+++ Accessors/TheNewsReporter.Accessors.UserPreferencesService/Models/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace TheNewsReporter.Accessors.UserPreferencesService.Models;
+
+public class DbContext
+{
+    public readonly IMongoCollection<UserPreferences> UserPreferencesCollection;
+
+    public DbContext(IOptions<MongoDbSettings> options)
+    {
+        var _mongoClient = new MongoClient(options.Value.ConnectionString);
+        UserPreferencesCollection = _mongoClient
+            .GetDatabase(options.Value.DatabaseName)
+            .GetCollection<UserPreferences>(options.Value.CollectionName);
+
+        var indexOptions = new CreateIndexOptions { Unique = true };
+        var indexKeysDefinition = Builders<UserPreferences>.IndexKeys.Ascending(x => x.UserId);
+        var indexModel = new CreateIndexModel<UserPreferences>(indexKeysDefinition, indexOptions);
+
+        UserPreferencesCollection.Indexes.CreateOne(indexModel);
+    }
+}
+
