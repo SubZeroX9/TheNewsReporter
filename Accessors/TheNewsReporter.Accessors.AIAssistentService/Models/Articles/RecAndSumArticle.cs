b96c9c9f7bd94f02e115cd1da012e065666f5e26
Rafael Azriaiev <84143075+SubZeroX9@users.noreply.github.com>
2024-07-11 18:29:49 +0300

    Added AI Assistant Service (#4)
    * Created AI Assisstent Service and configured lunch settings and added to docker compose
    
    * Updated AI Assitant Service
    
    - removed Weather template
    - created and added Models based on response and what is required
    - Added 3 endpoints for recomdations, summerize and one for both at the same time
    - Added Setting for AI api and updated apisettings

---
+++ Accessors/TheNewsReporter.Accessors.AIAssistentService/Models/Articles/RecAndSumArticle.cs
@@ -0,0 +1,10 @@
+﻿namespace TheNewsReporter.Accessors.AIAssistentService.Models.Articles
+{
+    public class RecAndSumArticle
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string Summary { get; set; }
+    }
+}
---
+++ Accessors/TheNewsReporter.Accessors.AIAssistentService/Models/Requests/AIAssistantRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using TheNewsReporter.Accessors.AIAssistentService.Models.Articles;
+
+namespace TheNewsReporter.Accessors.AIAssistentService.Models.Requests
+{
+    public class AIAssistantRequest
+    {
+        [JsonPropertyName("user_preferences")]
+        public UserPreferences UserPreferences { get; set; }
+        public List<NewsArticle> Articles { get; set; }
+    }
+}
