f174a2a19978fc7a5a4c3ab116cd783fe92877d6
Rafael Azriaiev <rafael.azrv@gmail.com>
2024-07-11 17:05:20 +0300

    Updated AI Assitant Service
    - removed Weather template
    - created and added Models based on response and what is required
    - Added 3 endpoints for recomdations, summerize and one for both at the same time
    - Added Setting for AI api and updated apisettings

---
+++ Accessors/TheNewsReporter.Accessors.AIAssistentService/Models/Articles/SummarizedArticle.cs
@@ -0,0 +1,9 @@
+﻿namespace TheNewsReporter.Accessors.AIAssistentService.Models.Articles
+{
+    public class SummarizedArticle
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Summary { get; set; }
+    }
+}
---
+++ Accessors/TheNewsReporter.Accessors.AIAssistentService/Models/UserPreferences.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheNewsReporter.Accessors.AIAssistentService.Models
+{
+    public class UserPreferences
+    {
+        public List<string> Categories { get; set; }
+        
+        [JsonPropertyName("news_preferences")]
+        public List<string> NewsPreferences { get; set; }
+    }
+}
