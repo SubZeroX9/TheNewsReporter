134034f27f898c98a30b0903b5654c4b2f528e77
Rafael Azriaiev <rafael.azrv@gmail.com>
2024-07-14 13:44:45 +0300

    Updated Notification Service With one Service and one endpoint
    - removed weather template
    - created MailGun Email Notification Service
    - Added MailGun Api settings
    - create scafold for future services
    - 1 endpoint for sending notifications

---
+++ Accessors/TheNewsReporter.Accessors.NotificationService/Models/CommunicationChannel.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheNewsReporter.Accessors.NotificationApiService.Models
+{
+    public enum Channel
+    {
+        NULL,
+        Email,
+        Sms,
+        PushNotification,
+        InAppNotification,
+        Telegram,
+        Slack,
+        Discord,
+        WhatsApp
+    }
+
+    public class CommunicationChannel
+    {
+        [JsonPropertyName("channel")]
+        public required string ComChannel
+        {
+            get => Enum.GetName(typeof(Channel), ChannelEnum);
+            set
+            {
+                if (value != null && Enum.TryParse(value, true, out Channel result))
+                {
+                    ChannelEnum = result;
+                }
+                else
+                {
+                    ChannelEnum = Channel.NULL;
+                }
+            }
+        }
+
+        [JsonIgnore]
+        public Channel ChannelEnum { get; set; } = Channel.NULL;
+
+        public required Dictionary<string, string> Details { get; set; }
+    }
+}
---
+++ Accessors/TheNewsReporter.Accessors.NotificationService/Services/NotificationService.cs
@@ -0,0 +1,59 @@
+﻿using TheNewsReporter.Accessors.NotificationApiService.Models;
+
+namespace TheNewsReporter.Accessors.NotificationApiService.Services
+{
+    public class NotificationService
+    {
+        private readonly ILogger<NotificationService> _logger;
+        private readonly Dictionary<Channel, INotificationService> _services;
+
+        public NotificationService(ILogger<NotificationService> logger, MailgunNotificationService mailgunNotificationService)
+        {
+            _logger = logger;
+            _services = new Dictionary<Channel, INotificationService>
+            {
+                { Channel.Email, mailgunNotificationService }
+            };
+        }
+
+        public async Task<bool> SendNotificationAsync(NotificationRequest notificationRequest)
+        {
+            Channel channel = notificationRequest.Channel.ChannelEnum;
+            _logger.LogInformation($"Sending notification to {channel} channel. In Notification Service");
+
+            if (!_services.ContainsKey(channel))
+            {
+                _logger.LogError($"Channel {channel} is not supported.");
+                return false;
+            }
+
+            if (!IsValidDetails(notificationRequest.Channel))
+            {
+                _logger.LogError($"Invalid details for {channel} channel.");
+                return false;
+            }
+
+            bool result = await _services[channel].SendNotificationAsync(notificationRequest);
+
+            _logger.LogInformation($"Notification sent to {channel} channel. In Notification Service");
+            return result;
+        }
+
+        private bool IsValidDetails(CommunicationChannel communicationChannel) {
+            return communicationChannel.ChannelEnum switch
+            {
+                Channel.Email => communicationChannel.Details.ContainsKey("email"),
+                Channel.Sms => communicationChannel.Details.ContainsKey("phoneNumber"),
+                Channel.PushNotification => communicationChannel.Details.ContainsKey("peviceId"),
+                Channel.InAppNotification => communicationChannel.Details.ContainsKey("userId"),
+                Channel.Telegram => communicationChannel.Details.ContainsKey("telegramId"),
+                Channel.Slack => communicationChannel.Details.ContainsKey("slackId"),
+                Channel.Discord => communicationChannel.Details.ContainsKey("discordId"),
+                Channel.WhatsApp => communicationChannel.Details.ContainsKey("whatsAppId"),
+                _ => false,
+            };
+        }
+
+
+    }
+}
