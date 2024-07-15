b25846121f501ac346db033a971ef6b0edc60e55
Rafael Azriaiev <84143075+SubZeroX9@users.noreply.github.com>
2024-07-15 14:32:39 +0300

    Created News Api Manager with Notification Service (#5)
    * Created News Api Manager Solution With template and swagger with docker-compose updated
    
    * Added Dapr support with config file
    
    - added 1 endpoint to test dapr using user prefs service to get all user pref
    - updated docker-compose to include rabbitmq for futer use and user pref queue component
    - updated docker-compose to include dapr side car for user prefs service and manager
    - added empty services in news api manager for newsaggregation and AI assistant
    - removed weather template
    
    * Updated UserPrefferences endpoints and added JsonPropertyName to requests and respond
    
    * Update NewApiManager with 3 endpoints
    
    - get all user preferences endpoint
    - set user preferences endpoint
    - update user preferences endpoint
    - get news for user returns accepted (Only gets user pref by id and his news - needs update for recommendations and summeries)
    
    * NewsApiManager now uses AIAssitant for recomndation and summerizing
    
    * Created and cofigured Notification Service Solution with Weather template
    
    * Updated seeder with valid email for testing
    
    * Updated Notification Service With one Service and one endpoint
    
    - removed weather template
    - created MailGun Email Notification Service
    - Added MailGun Api settings
    - create scafold for future services
    - 1 endpoint for sending notifications
    
    * Updated News Api Manager with notification service
    
    - added notification sidecar
    - added notification pub sub and subscription
    - added notification service to Manager which publishes the Notification to Notification Accessor
    - Added Message Builder for The Message in Notification Service
    - updated Classes with JsonPropertyNames
    - Updated Notification Accessor endpoint to /sendnotification and added Topic and name
    - Updated sendnotification endpoint to Recive CloudEvent of type NotificationRequest to recive PUBSUBs
    
    * Updated Licence added Readme for api and add example .env file for root

---
+++ Accessors/TheNewsReporter.Accessors.NotificationService/Services/MailgunNotificationService .cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using TheNewsReporter.Accessors.NotificationApiService.Models;
+
+namespace TheNewsReporter.Accessors.NotificationApiService.Services
+{
+    public class MailgunNotificationService : INotificationService
+    {
+        private readonly ILogger<MailgunNotificationService> _logger;
+        private readonly HttpClient _client;
+        private readonly MailGunApiSettings _settings;
+
+        public MailgunNotificationService(IOptions<MailGunApiSettings> options, ILogger<MailgunNotificationService> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _client = httpClient;
+            _settings = options.Value;
+            SetHttpClientParams();
+            //_logger.LogInformation("Api key: {apikey}",_settings.ApiKey);
+        }
+
+        private void SetHttpClientParams()
+        {
+            _client.BaseAddress = new Uri($"{_settings.BaseUrl}/{_settings.Domain}/messages");
+            var byteArray = Encoding.ASCII.GetBytes($"api:{_settings.ApiKey}");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(@"Basic", Convert.ToBase64String(byteArray));
+        }
+
+        public async Task<bool> SendNotificationAsync(NotificationRequest notificationRequest)
+        {
+            _logger.LogInformation("Sending notification using MailGun Service");
+            try
+            {
+                var recipient = notificationRequest.Channel.Details["email"];
+
+                MultipartFormDataContent content = new MultipartFormDataContent
+                {
+                    { new StringContent($"{_settings.DisplayName}  <news_reporter@{_settings.Domain}>"), "from" },
+                    { new StringContent(recipient), "to" },
+                    { new StringContent("Latest News"), "subject" },
+                    { new StringContent(notificationRequest.Message), "text" }
+                };
+
+
+                var request = await _client.PostAsync(string.Empty, content);
+                request.EnsureSuccessStatusCode();
+
+                string response = await request.Content.ReadAsStringAsync();
+
+                _logger.LogInformation($"Notification sent successfully. Response: {JsonSerializer.Serialize(response)}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Failed to send notification, ex: {ex}", ex.Message);
+
+                var innestException = ex.InnerException;
+
+                while (innestException != null)
+                {
+                    _logger.LogError("Inner exception: {innestException}", innestException.Message);
+                    innestException = innestException.InnerException;
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ Managers/TheNewsReporter.Managers.NewsApiManager/Models/UserPreferences/UserPreference.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheNewsReporter.Managers.NewsApiManager.Models.UserPreferences
+{
+    public class UserPreference
+    {
+        public string? Id { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string? UserId { get; set; }
+
+        [JsonPropertyName("prefered_categories")]
+        public List<string>? Categories { get; set; }
+
+        [JsonPropertyName("news_preferences")]
+        public List<string>? NewsPreferences { get; set; }
+
+        [JsonPropertyName("communication_channel")]
+        public CommunicationChannel? CommunicationChannel { get; set; }
+    }
+}
